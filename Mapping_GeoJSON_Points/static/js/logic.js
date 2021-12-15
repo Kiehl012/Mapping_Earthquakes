d59b75ffe36b308b7f2aa3b87d33412c323a6e67
Kiehl012 <jlkiehl10@gmail.com>
2021-12-15 17:59:41 -0600

    adding GeoJSON folder

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,24 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Grabbing our GeoJSON data.
+L.geoJSON(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer)
+    layer.bindPopup("<h3>Airport Code: "+ feature.properties.faa + "</h3><hr><h3>Airport Name: " + feature.properties.name + "</h3>");
+  }
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
